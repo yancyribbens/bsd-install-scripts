b2c218ff162a4d33a62ec55c9f027c0e77df5495
yancy <email@yancy.lol>
2022-01-24 19:03:21 +0100

    install-jekyl.sh

---
+++ install-jekyl.sh
@@ -0,0 +1,23 @@
+# bash
+cd /usr/ports/shells/bash
+make -DBATCH install clean
+
+# vim
+cd /usr/ports/editors/vim
+make -DBATCH install clean
+
+# sudo
+cd /usr/ports/secuirty/sudo
+make -DBATCH install clean
+
+# git
+cd /usr/ports/devel/git
+make -DBATCH install clean
+
+# bundler
+cd /usr/ports/sysutils/rubygem-bundler
+make -DBATCH install clean
+
+# jekyll
+cd /usr/ports/www/rubygem-jekyll
+make -DBATCH install clean
